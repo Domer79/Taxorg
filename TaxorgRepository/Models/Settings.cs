7144babc25c5f961f82e75cd0e3bc5522a2fc3bd
Гарипов Дамир Сагдиевич <garipovd@mail.ru>
2015-03-02 19:53:55 +0500

    csv

---
+++ TaxorgRepository/Models/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxorgRepository.Models
+{
+    [Table("Settings")]
+    public class Settings : ModelBase
+    {
+        public const string TaxPeriodYear = "taxperiodyear";
+        public const string TaxPeriodMonth = "taxperiodmonth";
+        public const string TaxPrevPeriod = "taxprevperiod";
+        public const string IsNotSameTaxLoad = "isnotsametaxload";
+
+        [Key]
+        public int IdSettings { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Description { get; set; }
+    }
+}
