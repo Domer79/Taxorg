9c6c0e3dfb32ab8c195981c667c1bc3949178322
Domer79 <garipovd@mail.ru>
2015-03-02 08:50:59 +0500

    dfgdsfg

---
+++ TaxorgRepository/Models/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxorgRepository.Models
+{
+    [Table("Settings")]
+    public class Settings
+    {
+        public int IdSettings { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Description { get; set; }
+    }
+}
