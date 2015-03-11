31626915d3981579a17a31c50fc7b04121a41d0c
Гарипов Дамир Сагдиевич <garipovd@mail.ru>
2015-03-11 23:44:35 +0500

    Settings & StringExtentions
    Исправил отображение настроек, доработал SplitReverse в StringExtensions

--- WebTools/Extensions/StringExtensions.cs
+++ WebTools/Extensions/StringExtensions.cs
@@ -8,14 +8,18 @@ namespace SystemTools.Extensions
 {
     public static class StringExtensions
     {
-        public static string SplitReverse(this IEnumerable<object> args)
+        public static string SplitReverse(this IEnumerable<object> args, string delimiter)
         {
             if (args == null)
                 return String.Empty;
 
             var enumerable = args as object[] ?? args.ToArray();
-            var splitResult = !enumerable.Any() ? String.Empty : enumerable.Select(arg => arg.ToString()).Aggregate((str, next) => str + ", " + next);
+            var splitResult = !enumerable.Any() ? String.Empty : enumerable.Select(arg => arg.ToString()).Aggregate((str, next) => str + delimiter + next);
             return splitResult;
+        }
+        public static string SplitReverse(this IEnumerable<object> args)
+        {
+            return SplitReverse(args, ", ");
         }
     }
 }
