760f83c557dab5eb40a99b0793a30bcaa966e16d
Domer79 <garipovd@mail.ru>
2015-04-24 12:51:49 +0500

    24.04.2015 обед

---
+++ SystemTools/WebTools/Attributes/ActionAliasAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SystemTools.WebTools.Attributes
+{
+    public abstract class AliasAttributeBase : Attribute
+    {
+        private readonly string _alias;
+        private readonly string _description;
+
+        protected AliasAttributeBase(string @alias) 
+            : this(alias, null)
+        {
+        }
+
+        protected AliasAttributeBase(string @alias, string description)
+        {
+            _alias = alias;
+            _description = description;
+        }
+
+        public string Alias
+        {
+            get { return _alias; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ActionAliasAttribute : AliasAttributeBase
+    {
+        public ActionAliasAttribute(string @alias) 
+            : base(alias)
+        {
+        }
+
+        public ActionAliasAttribute(string @alias, string description) 
+            : base(alias, description)
+        {
+        }
+    }
+
+    public class EntityAliasAttribute : AliasAttributeBase
+    {
+        public EntityAliasAttribute(string alias) 
+            : base(alias)
+        {
+        }
+
+        public EntityAliasAttribute(string alias, string description) 
+            : base(alias, description)
+        {
+        }
+    }
+}
